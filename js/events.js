87b13fa6f2c2c70c2037a21b20a67a7649852d53
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 03:47:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function() {
-    document.getElementById('img').className = 'tasty';
+    document.getElementsByTag('img').className = 'tasty';
   });
 }
 
