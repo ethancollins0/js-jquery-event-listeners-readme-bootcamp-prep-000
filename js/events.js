9e4dc2e62e50df21ad1e5f6362bc4910d3308670
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 03:33:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function pressIt() {
 
 function submitIt() {
   $('input:submit').on('click', function() {
-    alert("Your form is going to be submitted now.")
+    $(alert("Your form is going to be submitted now."))
   });
   return;
 }
