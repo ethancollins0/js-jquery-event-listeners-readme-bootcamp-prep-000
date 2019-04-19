61cb56231c7bc47e40ccd1824b8a1543f91f3f66
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 03:33:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keypress', function(key) {
-    if (key.which == 71) {
+    if $(key.which == 71) {
       alert('You pressed G')
     }
   });
